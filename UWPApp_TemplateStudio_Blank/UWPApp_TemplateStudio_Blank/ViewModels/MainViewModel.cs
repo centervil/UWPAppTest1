1463fe3ad2808d96ed91005deb857970c8f978b3
ak-nkmr <centervil.en@gmail.com>
2019-04-07 22:39:49 +0900

    CreateBlankProjectByTemplateStudio

---
+++ UWPApp_TemplateStudio_Blank/UWPApp_TemplateStudio_Blank/ViewModels/MainViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+using UWPApp_TemplateStudio_Blank.Helpers;
+
+namespace UWPApp_TemplateStudio_Blank.ViewModels
+{
+    public class MainViewModel : Observable
+    {
+        public MainViewModel()
+        {
+        }
+    }
+}
---
+++ UWPApp_TemplateStudio_Blank/UWPApp_TemplateStudio_Blank/Views/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using UWPApp_TemplateStudio_Blank.ViewModels;
+
+using Windows.UI.Xaml.Controls;
+
+namespace UWPApp_TemplateStudio_Blank.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainViewModel ViewModel { get; } = new MainViewModel();
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
