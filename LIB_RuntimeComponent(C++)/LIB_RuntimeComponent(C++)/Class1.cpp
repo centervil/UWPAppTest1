b8d9e56d828d7193e2b159332632e72befbde263
ak-nkmr <centervil.en@gmail.com>
2019-05-04 01:33:13 +0900

    CreateWinRTComponent(C++DLL)Project

---
+++ LIB_RuntimeComponent(C++)/LIB_RuntimeComponent(C++)/Class1.cpp
@@ -0,0 +1,9 @@
+﻿#include "pch.h"
+#include "Class1.h"
+
+using namespace LIB_RuntimeComponent_C___;
+using namespace Platform;
+
+Class1::Class1()
+{
+}
---
+++ LIB_RuntimeComponent(C++)/LIB_RuntimeComponent(C++)/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace LIB_RuntimeComponent_C___
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+    };
+}
