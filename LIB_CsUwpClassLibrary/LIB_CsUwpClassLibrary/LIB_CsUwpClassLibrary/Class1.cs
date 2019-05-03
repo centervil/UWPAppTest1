91baaf62472117d9b0d7b4717acb12a62794c9b4
ak-nkmr <centervil.en@gmail.com>
2019-05-04 01:22:59 +0900

    CreateUwpDLL(C#)Project

---
+++ LIB_CsUwpClassLibrary/LIB_CsUwpClassLibrary/LIB_CsUwpClassLibrary/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIB_CsUwpClassLibrary
+{
+    public class Class1
+    {
+    }
+}
