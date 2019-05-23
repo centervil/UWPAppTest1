4c6a1fcf305ce62b13dea4b41453ebeb2967faa0
ak-nkmr <centervil.en@gmail.com>
2019-05-23 11:44:14 +0900

    NetStandardライブラリからCppDllとCppUwpDllのAPIを呼び出すコードを実装

--- LIB_NetStdClassLibrary/LIB_NetStdClassLibrary/Class1.cs
+++ LIB_NetStdClassLibrary/LIB_NetStdClassLibrary/Class1.cs
@@ -1,9 +1,32 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+
 
 namespace LIB_NetStdClassLibrary
 {
     public class Class1
     {
         public static string TestMethod() { return "NetStandardDLL_Test"; }
+    }
+    public class NativeMethods
+    {
+        [DllImport("LIB_CppClassLibrary", EntryPoint = "TestMethod")]
+        internal static extern Int32 TestMethod();
+
+        [DllImport("LIB_CppUwpDLL", EntryPoint = "TestMethod")]
+        internal static extern Int32 TestMethodUwp();
+
+        public static string InvokeCppDLL()
+        {
+            int num = TestMethod();
+            return num.ToString();
+        }
+
+        public static string InvokeCppUwpDLL()
+        {
+            int num = TestMethodUwp();
+            return num.ToString();
+        }
+
     }
 }
